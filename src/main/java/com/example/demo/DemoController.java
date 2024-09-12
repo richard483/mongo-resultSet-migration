8a3dfd7125be310a65adb7af7c5e82f11547c550
richard483 <richard.william483@gmail.com>
2024-09-12 10:35:43 +0700

    updated colleciton name on mongoTemplate action to be parameterized

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.mongodb.client.result.DeleteResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -12,13 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+  @Value("${spring.data.mongodb.collection}")
+  private String collectionName;
+
   @Autowired
   MongoTemplate mongoTemplate;
 
   @Transactional
   @PostMapping("/save")
   public Book save(@RequestBody Book book) {
-    return mongoTemplate.save(book, "book");
+    return mongoTemplate.save(book, collectionName);
   }
 
   @Transactional
@@ -26,7 +30,7 @@ public class DemoController {
   public DeleteResult delete(@RequestBody String id) {
     Query query = new Query();
     query.addCriteria(Criteria.where("id").is(id));
-    return mongoTemplate.remove(query, "book");
+    return mongoTemplate.remove(query, collectionName);
   }
 
 
